2443ee2b1e58ba0d7d8409df6e16c4582be599f6
serlinecai4935 <scai4935@bths.edu>
2018-11-25 19:22:54 -0500

    a

--- src/MenuRunner.java
+++ src/MenuRunner.java
@@ -9,6 +9,10 @@ public class MenuRunner {
 
         Trio trio1= new Trio(reuben, waldorf, icedTea);
         Trio trio2= new Trio (bec, caesar, smoothie);
-        Trio trio3= new Trio(bec, caesar, icedTea)
+        Trio trio3= new Trio(bec, caesar, icedTea);
+
+        System.out.println(trio1. getName()+ "Price:" + trio1.getPrice() );
+        System.out.println(trio2.getName()+ "Price:" + trio2.getPrice());
+        System.out.println(trio3.getName()+ "Price:" + trio3.getPrice());
     }
 }
